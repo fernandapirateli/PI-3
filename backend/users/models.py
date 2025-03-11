0f5eb00d5a4ab5fd0d240e2214119de0c41f148e
Thiago P.L <thiagopl84@gmail.com>
2025-03-11 18:11:10 -0300

    feat: criada a classe User

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_type = models.CharField(max_length=15)
+
+    class Meta:
+        verbose_name_plural = 'Usuários'
+
+    def __str__(self):
+        return self.first_name
